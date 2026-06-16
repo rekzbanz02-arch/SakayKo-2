59ca7fdb8d8a739a1e15cdfb4c3327add0117a23
rekzbanz <rekzbanz02@gmail.com>
2026-06-16 12:04:38 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'sakayko-v10-5-pwa-cache-v1';
+const APP_SHELL = [
+  './',
+  './SakayKo_v10_5_PWA_Chrome_install_fixed.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL).catch(() => undefined)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
+    await self.clients.claim();
+  })());
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith((async () => {
+    const cached = await caches.match(event.request);
+    if (cached) return cached;
+    try {
+      const response = await fetch(event.request);
+      const cache = await caches.open(CACHE_NAME);
+      if (response && response.ok && event.request.url.startsWith(self.location.origin)) {
+        cache.put(event.request, response.clone());
+      }
+      return response;
+    } catch (err) {
+      return caches.match('./SakayKo_v10_5_PWA_Chrome_install_fixed.html');
+    }
+  })());
+});
